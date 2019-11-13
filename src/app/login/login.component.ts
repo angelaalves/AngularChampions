e1389fb54e04a33a3762b48e05c1cc0e796697b8
Carolina Martins <carolina.martins@polarising.com>
2019-11-13 12:07:02 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -61,45 +61,4 @@ export class LoginComponent implements OnInit {
     );
     form.reset();
   }
-
-
-  /*signup(email: string, password: string) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8085/players/Login?email=${email}&password=${password}', true);
-    return this.http.post<AuthResponseData>('http://localhost:8085/players/Login',
-      {
-        email: email,
-        password: password
-      }
-    ).pipe(catchError(this.handleError),
-      tap(resData => {
-        this.handleAuthentication(resData.email, resData.localId)
-      })
-    );
-  }*/
-
-  private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = 'an unknown error occurred';
-    if (!errorRes.error || !errorRes.error.error) {
-      return throwError(errorMessage);
-    }
-    switch (errorRes.error.error.message) {
-      case 'EMAIL_EXISTS':
-        errorMessage = 'this email already exists';
-        break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = 'This email does not exist';
-        break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'This password is not correct';
-        break;
-    }
-    return throwError(errorMessage);
-  }
-
-  private handleAuthentication(email: string, userId: string) {
-    const expirationDate = new Date(new Date().getTime());
-    //const player = new Player(name,email, userId, playerType.GuildMaster );
-    //this.user.next(player);
-  }
 }
