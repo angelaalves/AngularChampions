f0d0aacf86cd734952a8e9cb0f218f0c0953bda5
Svitlana <svitlana.palona@polarising.com>
2019-11-19 10:07:10 +0000

    Html2

--- src/app/login/authentication/authentication.service.ts
+++ src/app/login/authentication/authentication.service.ts
@@ -3,13 +3,13 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 import { Player } from 'src/app/shared/player.model';
-
+ 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
   user = new Subject<Player>();
-
+ 
   constructor(private http: HttpClient) { }
-
+ 
   signup(email: string, password: string) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8085/players/Login?email='+email+'&password='+password, true); 
@@ -21,10 +21,17 @@ export class AuthenticationService {
     ).pipe(catchError(this.handleError),
       tap(resData => {
         this.handleAuthentication(resData.idPlayer, resData.idGuild, resData.UserName, resData.email, resData.password,  resData.gender, resData.userType, resData.xp, resData.ChampiesToGive, resData.MyChampies, resData.Status )
-      })         
+   
+      })   
     );
-  }
 
+
+
+
+
+ 
+  }
+ 
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = 'an unknown error occurred';
     if (!errorRes.error || !errorRes.error.error) {
@@ -42,5 +49,11 @@ export class AuthenticationService {
         break;
     }
     return throwError(errorMessage);
-   }
+  }
+ 
+  private handleAuthentication(idPlayer:string, idGuild: string, UserName:string, email:string, password:string,  gender:string, userType: string, xp: string, ChampiesToGive:string, MyChampies: string, Status: string) {
+    const expirationDate = new Date(new Date().getTime());
+    //const player = new Player(name,email, userId, playerType.GuildMaster );
+    //this.user.next(player);
+  }
 }
