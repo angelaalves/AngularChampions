0cbf1732dd5333cfbd95d7a4a1828eb3ccae5bfc
Svitlana <svitlana.palona@polarising.com>
2019-11-21 16:10:24 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/app/closet/add-skin/add-skin.component.ts
+++ src/app/closet/add-skin/add-skin.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl, Validators, NgForm } from '@angular/forms';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators, NgForm, FormBuilder } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,10 +8,12 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './add-skin.component.html',
   styleUrls: ['./add-skin.component.css']
 })
+
+@Injectable({providedIn:'root'})
 export class AddSkinComponent implements OnInit {
   addSkinForm: FormGroup;
 
-  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private router: Router, private route: ActivatedRoute, private formBuilder: FormBuilder,private http: HttpClient) { }
 
   ngOnInit() {
     this.route.params
@@ -26,30 +28,37 @@ export class AddSkinComponent implements OnInit {
     if (!form.valid) {
       return;
     }
+
+    const idSkin = "1";
     const skinName = form.value.skinName;
     const imagePath = form.value.filePath;
-    const minXP = form.value.mininumXP;
+    const minXP = form.value.minXP;
     const champiesCost = form.value.numberOfChampies;
     const skinType = form.value.skinType;
-    console.log('addUserForm', form.value);
 
-    /*var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8085/skins/Create?skinName=' + skinName + '&imagePath=' + imagePath + '&minXP=' + minXP +
+    console.log('addUserForm', form.value);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8085/skins/Create?idSkin=' + idSkin + '&skinName=' + skinName + '&imagePath=' + imagePath + '&minXP=' + minXP +
       '&champiesCost=' + champiesCost + '&skinType=' + skinType, true);
-    this.http.post<any>('http://localhost:8085/skins/Create?skinName=' + skinName + '&imagePath=' + imagePath + '&minXP=' + minXP +
+    this.http.post<any>('http://localhost:8085/skins/Create?idSkin=' + idSkin + '&skinName=' + skinName + '&imagePath=' + imagePath + '&minXP=' + minXP +
       '&champiesCost=' + champiesCost + '&skinType=' + skinType,
       {
+        idSkin,
         skinName,
         imagePath,
         minXP,
         champiesCost,
         skinType
       }
-    ).subscribe();*/
-
+    ).subscribe();
 
     console.log(this.addSkinForm);
-    //this.router.navigate(['/ancient_profile'], { relativeTo: this.route });
+
+    this.router.navigate(['/ancient_profile'], {relativeTo: this.route});
+
+
+   
   }
 
   private initForm() {
