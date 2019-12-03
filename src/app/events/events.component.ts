060f917ee16dcc9b6a2b6d029e9a781ae37b5645
Tiago <tiago.martins.santos@polarising.com>
2019-12-03 14:17:50 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from './event.model';
 import { eventType } from './event-type.enum';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-events',
@@ -8,21 +9,17 @@ import { eventType } from './event-type.enum';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  events: Event[]=[
-    new Event(new Date(),'Checkpoint',eventType.Checkpoint),
-    new Event(new Date(),'Kickoff',eventType.KickOff),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner),
-    new Event(new Date(),'jantar de Natal',eventType.ChristmasDinner)
-  ];
+  events: Event[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.post<any[]>('http://localhost:8085/events/getAll', {}).subscribe(eventsData => {
+      console.log(eventsData)
+      this.events = eventsData
+      console.log(this.events)
+    }
+    )
   }
 
 }
