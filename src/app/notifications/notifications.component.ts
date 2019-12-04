c6d835ee2135a32d70b5cbc1641291b22a704e62
angela.alves <angela.alves@polarising.com>
2019-12-04 12:37:43 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -13,44 +13,78 @@ import { ListenerService } from '../services/listener.service';
 })
 
 
-
 @Injectable({ providedIn: 'root' })
 export class NotificationsComponent implements OnInit {
-public notificationreceiver: NotificationsReceivers[];
-public notviewed:Notification[];
-public viewed:Notification[];
-public notificationEl: NotificationsReceivers;
-  constructor(private router: Router, private route: ActivatedRoute,private session: SessionService, private http: HttpClient, private authService: AuthenticationService, private listener: ListenerService) { 
-  this.getNotifications();
+  public notificationreceiver: NotificationsReceivers[];
+  public notificationids: String[];
+  public notviewed: Notification[];
+  public all: Notification[];
+  constructor(private router: Router, private route: ActivatedRoute, private session: SessionService, private http: HttpClient, private authService: AuthenticationService) {
+
   }
 
   ngOnInit() {
-    
+    this.getNotifications();
   }
 
-  redirectToProfile(){
-    this.router.navigate(['../ancient_profile'], {relativeTo: this.route});
+  redirectToProfile() {
+    //Fazeraqui
+    for (let idsofnoti of this.notificationids) {
+      console.log("idsofnoti ");
+      console.log(idsofnoti);
+      this.http.get<NotificationsReceivers[]>('http://localhost:8085/notificationreceivers/Get?ID_Notification_FK=' + idsofnoti).subscribe(dataaux => {
+        const notificationnowseen = dataaux[0];
+        const idNotificationReceiver = notificationnowseen.idnotificationReceiver;
+        console.log("notificationnowseen.idnotificationReceiver");
+        console.log(notificationnowseen.idnotificationReceiver);
+        const idPlayerReceiverFK = notificationnowseen.idplayerReceiverFK;
+        console.log("notificationnowseen.idplayerReceiverFK");
+        console.log(notificationnowseen.idplayerReceiverFK);
+        const ID_Notification_FK = notificationnowseen.idnotificationFK;
+        console.log("notificationnowseen.idnotificationFK");
+        console.log(notificationnowseen.idnotificationFK);
+        const ID_Guild_FK = notificationnowseen.idguildFK;
+        console.log("notificationnowseen.idguildFK");
+        console.log(notificationnowseen.idguildFK);
+        const notificationSeen = "true";
+        console.log("notificationSeen");
+        console.log(notificationSeen);
+
+
+        this.http.post<NotificationsReceivers>('http://localhost:8085/notificationreceivers/Update?idNotificationReceiver=' + idNotificationReceiver + '&idPlayerReceiverFK=' + idPlayerReceiverFK + 
+        '&ID_Notification_FK=' + ID_Notification_FK + '&ID_Guild_FK=' + ID_Guild_FK + '&notificationSeen=' + notificationSeen, 
+        { idNotificationReceiver, idPlayerReceiverFK, ID_Notification_FK, ID_Guild_FK, notificationSeen }).subscribe(resData => {
+          console.log(resData);
+        });
+      });
+
+    }
+    this.router.navigate(['../ancient_profile'], { relativeTo: this.route });
   }
 
 
-  getNotifications(){
-    this.notviewed=[];
+  getNotifications() {
+    this.notviewed = [];
     this.http.get<NotificationsReceivers[]>('http://localhost:8085/notificationreceivers/Get?idPlayerReceiverFK=' + this.session.playerSession.idplayer).subscribe(data => {
       this.notificationreceiver = data;
-      
+      this.notificationids = [];
+      for (let noti of this.notificationreceiver) {
+        if (noti.notificationSeen == "false") {
+          this.notificationids.push(noti.idnotificationFK);
+          console.log("IOd da notificação");
+          console.log(noti.idnotificationFK);
+        }
+      }
       console.log("notifications");
       console.log(this.notificationreceiver);
       console.log(this.notificationreceiver[0].idnotificationFK)
 
-      for (let notification of this.notificationreceiver) {
-        this.notificationEl=notification;
-        console.log(notification)
-        console.log(this.notificationEl)
-        console.log(this.notificationEl.idnotificationFK);
-        this.http.get<Notification[]>('http://localhost:8085/notifications/Get?ID_Notification=' + notification.idnotificationFK).subscribe(data => {
-          
-        //falta a filtragem d o not viwed
-        this.notviewed.push(data[0]);
+      for (let id of this.notificationids) {
+        console.log(id);
+        this.http.get<Notification[]>('http://localhost:8085/notifications/Get?ID_Notification=' + id).subscribe(data => {
+
+          //falta a filtragem d o not viwed
+          this.notviewed.push(data[0]);
           console.log("notifications not viewed");
           console.log(this.notviewed);
         });
--- src/app/shared/notificationsReceivers.model.ts
+++ src/app/shared/notificationsReceivers.model.ts
@@ -1,19 +1,19 @@
 import { typeWithParameters } from '@angular/compiler/src/render3/util';
 import { VirtualTimeScheduler } from 'rxjs';
 
-export class NotificationsReceivers {
-  idnotificationReceiver: String;
-  idplayerReceiverFK: String;
-  idnotificationFK: String;
-  idguildFK: String;
-  notificationSeen: String;
+export class NotificationsReceivers{
+idnotificationReceiver:String;
+   idplayerReceiverFK:String;
+     idnotificationFK:String;
+    idguildFK:String;
+  notificationSeen:String;
 
 
-  constructor(idNotificationReceiver: String, idPlayerReceiverFK: String, ID_Notification_FK: String, ID_Guild_FK: String, notificationSeen: String) {
-    this.idnotificationReceiver = idNotificationReceiver;
-    this.idplayerReceiverFK = idPlayerReceiverFK;
-    this.idnotificationFK = ID_Notification_FK;
-    this.idguildFK = ID_Guild_FK;
-    this.notificationSeen = notificationSeen;
-  }
+    constructor( idnotificationReceiver:String,  idplayerReceiverFK:String,  idnotificationFK:String, idguildFK:String, notificationSeen:String){
+        this.idnotificationReceiver=idnotificationReceiver;
+       this. idplayerReceiverFK= idplayerReceiverFK;
+       this.idnotificationFK=idnotificationFK;
+       this.idguildFK=idguildFK;
+       this. notificationSeen= notificationSeen;
+    }
 }
