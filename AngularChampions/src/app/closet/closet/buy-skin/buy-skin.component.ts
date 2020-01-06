e220fd6d2a5cb4146670e88bc20eb3524c95bdad
Carolina Martins <carolina.martins@polarising.com>
2020-01-06 12:06:55 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ AngularChampions/src/app/closet/closet/buy-skin/buy-skin.component.ts
@@ -0,0 +1,110 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
+import { Skin } from 'src/app/shared/skin.model';
+import { Player } from 'src/app/shared/player.model';
+import { SkinService } from 'src/app/services/skin.service';
+import { ModalService } from 'src/app/services/model.service';
+import Swal from 'sweetalert2'
+import { status } from 'AngularChampions/src/app/shared/status.enum';
+
+@Component({
+  selector: 'app-buy-skin',
+  templateUrl: './buy-skin.component.html',
+  styleUrls: ['./buy-skin.component.css']
+})
+
+@Injectable({ providedIn: 'root' })
+export class BuySkinComponent implements OnInit {
+  player: Player;
+  activeSkins: Skin[] = [];
+  shoppingCartSkins: Skin[] = [];
+  bodyText: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient, private session: SessionService,
+    private skinService: SkinService, private modalService: ModalService) {
+    this.player = this.session.getPlayerInSession();
+  }
+
+  ngOnInit() {
+    this.skinService.shoppingCartSkins.subscribe(shoppingCart => this.shoppingCartSkins = shoppingCart);
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+
+  redirectBackToCloset() {
+    this.router.navigate(['../closet'], { relativeTo: this.route });
+  }
+
+  removeItem(skin: Skin) {
+    this.skinService.removeFromShoppingCart(skin);
+  }
+
+  emptyCart() {
+    this.skinService.emptyCart();
+    this.redirectBackToCloset();
+  }
+
+  buySkin() {
+    const idplayer = this.session.getPlayerInSession().idplayer;
+
+    for (let item of this.shoppingCartSkins) {
+
+      var idSkin = item.idskin;
+
+      if (this.player.myChampies >= item.champiesCost && this.player.xp >= item.minXP) {
+        this.http.post<any>('http://localhost:8189/closet/Create?idSkinFK=' + idSkin + '&idPlayerFk=' + idplayer + '&status=',
+          {
+            idSkin,
+            idplayer,
+          }
+        ).subscribe();
+
+        this.activeSkins.push(item);
+
+        const myChampiesAfterBuyingSkin = Number(this.player.myChampies) - Number(item.champiesCost);
+
+        this.http.post<any>('http://localhost:8189/players/Update?idPlayer=' + idplayer + '&idGuildFK= &userName= &email= &password= &gender= &userType= &xp= &champiesToGive= &myChampies=' + myChampiesAfterBuyingSkin + '&status= ',
+          {
+            idplayer,
+            myChampiesAfterBuyingSkin
+          }
+        ).subscribe();
+
+        let counter: number = -1;
+        for (let activeSkin of this.activeSkins) {
+          counter++;
+          const skinID = activeSkin.idskin;
+          const skinStatus: status = status.Inactive;
+          if (activeSkin.skinType === item.skinType) {
+            this.http.post<any>('http://localhost:8189/closet/Update?idSkinFK=' + skinID + '&idPlayerFk=' + idplayer + '&status=' + skinStatus,
+              {
+                skinID,
+                idplayer,
+                skinStatus
+              }
+            );
+          }
+        }
+      } else if ((this.player.myChampies < item.champiesCost) || (this.player.xp < item.minXP)) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'You do not have enough champies and/or xp to purchase everything in your shopping cart',
+          footer: 'Please shorten your shopping cart'
+        });
+      }
+      this.player.changeImage(item.imagePath, item.skinType);
+      this.activeSkins.push(item);
+      this.skinService.emptyCart();
+      this.router.navigate(['../closet'], { relativeTo: this.route });
+    }
+  }
+}
--- src/app/services/skin.service.ts
+++ src/app/services/skin.service.ts
@@ -39,15 +39,16 @@ export class SkinService implements OnInit {
     }
 
     ngOnInit() {
-        this.http.get<Closet[]>('http://localhost:8085/closet/Get?idPlayerFk=' + this.player.idplayer + "&status=" + status, {}).subscribe(data => {
+        this.http.get<Closet[]>('http://localhost:8085/closet/Get?idPlayerFk=' + this.player.idPlayer + "&status=" + status, {}).subscribe(data => {
             console.log(data);
             for (let d of data) {
                 this.http.get<Skin>('http://localhost:8085/skins/Get=idSkin' + d.idskinFK).subscribe(resdata => {
-                    console.log(resdata);
+                    console.log("resdata ng on init do skin service:"+resdata);
                     this.skins.push(resdata);
+                    this.inactiveSkinsToBe.push(resdata);
+                    console.log("skin service: "+this.inactiveSkinsToBe);
                 });
             }
-            //transformar closet em skins e atribuir ao objeto this.inactiveSkins
         });
     }
 
@@ -73,7 +74,7 @@ export class SkinService implements OnInit {
         } else if (skinType.Others == skin.skinType) {
             index = 5;
         }
-        console.log(skin);
+        console.log(this.skins);
         this.inactiveSkinsToBe.push(this.skins[index]);
         console.log(this.inactiveSkinsToBe);
         this.newSkinsSelected.next(this.skins.splice(Number(index), 0, skin));
